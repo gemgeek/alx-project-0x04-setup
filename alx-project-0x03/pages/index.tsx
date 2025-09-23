58ddecdd6d982119960dc5e403b9868b06a2885a
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-23 22:15:04 +0100

    feat: import and apply Montserrat Google Font

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
    <div className="text-center pt-48">
-    <h1 className="text-4xl font-bold"> Welcome to Splash App </h1>
+    <h1> Welcome to Splash App </h1>
    </div>
   )
 }
