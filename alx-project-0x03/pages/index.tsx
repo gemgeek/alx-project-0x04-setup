7dac53ef73b38bcbe4d02ad6dc2b4adf68cdee31
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-23 22:39:16 +0100

    Update welcome message

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center px-4">
       {/* Welcome Message */}
-      <h1> Welcome to Splash App! </h1>
+      <h1> Welcome to Splash App </h1>
       <p className="text-lg text-gray-600 mb-8 max-w-2xl">
         Your one-stop platform for everything AI you need. Start exploring by
         navigating to our features below.
