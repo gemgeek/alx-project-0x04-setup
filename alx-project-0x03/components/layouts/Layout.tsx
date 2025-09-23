adc4ed69b7bd446dc99cf7fb90d37f0aa87e5ee6
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-23 21:57:20 +0100

    feat: implement shared layout and manually configure Tailwind v3

---
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow pt-24">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
